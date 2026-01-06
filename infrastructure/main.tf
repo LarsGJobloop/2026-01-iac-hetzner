882363e25ae1aa0036ffe37dd92f00dad32ce093
zabronax <zabronax@gmail.com>
2026-01-06 12:00:55 +0100

    define hetzner server

---
+++ infrastructure/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    hcloud = {
+      source = "hetznercloud/hcloud"
+      version = "1.58.0"
+    }
+  }
+}
+
+variable "hcloud_token" {
+  sensitive = true
+}
+
+provider "hcloud" {
+  token = var.hcloud_token
+}
+
+resource "hcloud_ssh_key" "example_ssh_key" {
+  name = "example-key"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEhEZoldAWf6T60aSvmlTFLWmQbMmTrWD6s5t55ezur3 "
+}
+
+resource "hcloud_server" "example_server" {
+  name = "example-server"
+
+  server_type = "cax41"
+  image = "debian-13"
+  location = "hel1" # Helsinki
+
+  ssh_keys = [
+    hcloud_ssh_key.example_ssh_key.id
+  ]
+}
+
+output "server_ipv4" {
+  value = hcloud_server.example_server.ipv4_address
+}
